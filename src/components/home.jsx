8dc6ea81ee12d866672ceb50c4d490add6c4c58b
Shourya <shouryasinha001@gmail.com>
2021-08-25 20:40:12 +0530

    Added fullscreen opt

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "../styles/home.css"
 import { Input } from "./input"
 export const Home = () => {
 
+    const [max, setMax] = useState(false);
+    useEffect(() => {
+        let head = document.getElementById("buttons")
+        let window = document.getElementById("window")
+        let input = document.getElementById("input")
+        if (max) {
+            window.setAttribute("style", `
+            width: 100vw;
+            height:100%;
+        `)
+
+            head.setAttribute("style", `width:100%`)
+
+            input.setAttribute("style", "width:100vw")
+
+        }
+        else {
+            window.setAttribute("style", `
+        width: 85vw;
+        height:85vh;
+        `)
+            input.setAttribute("style", "width:85vw")
+            head.setAttribute("style", "width:85vw")
+        }
+    }, [max])
+
     return (
         <div className="home-main">
-            <div className="buttons">
-                <img className="img" src="https://image.flaticon.com/icons/png/512/864/864393.png" alt="" />
-                <img src="https://image.flaticon.com/icons/png/512/136/136831.png" alt="" className="img" />
+            <div id="buttons">
+                <button className="img-hldr" >
+                    <img className="img" src="https://image.flaticon.com/icons/png/512/864/864393.png" alt="" />
+                </button>
+                <button className="img-hldr" onClick={() => setMax(!max)}>
+                    <img src="https://image.flaticon.com/icons/png/512/136/136831.png" alt="" className="img" />
+                </button>
             </div>
             <div id="window">
                 <p className="command">
